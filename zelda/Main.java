1187c90cb53258642a6b5aa0624dcb5360b6b838
NiRuX <perezkyle6@gmail.com>
2021-07-09 16:18:36 -0400

    Added back Debug Code
    Debug code caused it to crash after the swap to windowed.

--- zelda/Main.java
+++ zelda/Main.java
@@ -18,13 +18,14 @@ public class Main extends JFrame
 		if(game.isDebug())
 		{
 			setLocationRelativeTo(null);
-			//setSize(640,480); -- Old full Screen code that is now unused since we moved to windowed.
+			setSize(640, 480);
+			//Debug code will be updated in time.
 		}
 
-		setTitle("Center");
-
+		setSize(640, 480); // Code is now functional
+		setLocationRelativeTo(null); // Set to the center of the screen relative to x,y getSize.
+		setTitle(" Zelda - A Link To The Past [ * ] ");
 		setUndecorated(false);
-		setSize(640, 480); // << not working!!!
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 
