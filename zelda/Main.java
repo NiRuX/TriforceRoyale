ee78437ba6ef4476f832693c8eb3dbd6ac97d43c
NiRuX <perezkyle6@gmail.com>
2021-07-09 17:11:49 -0400

    Updated Class
    Updated Class Files and Improved the system.
    osDetection has been added to improve the handling of saving functionality to be added on 1.4d
    
    Improved Code Base.

--- zelda/Main.java
+++ zelda/Main.java
@@ -1,7 +1,12 @@
 package zelda;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+
+import sun.text.resources.cldr.ps.FormatData_ps;
 import zelda.engine.Game;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class Main extends JFrame
 {
@@ -24,7 +29,7 @@ public class Main extends JFrame
 
 		setSize(640, 480); // Code is now functional
 		setLocationRelativeTo(null); // Set to the center of the screen relative to x,y getSize.
-		setTitle(" Zelda - A Link To The Past [ * ] ");
+		setTitle(" Zelda - A Link To The Past [ * ] [ PR ] [ 1.1abz ]");
 		setUndecorated(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
@@ -35,6 +40,16 @@ public class Main extends JFrame
 
 	public static void main(String[] args)
 	{
+		final JFrame parent = new JFrame();
+		String name = JOptionPane.showInputDialog(parent,
+				"What is your name?", null);
+
+		// Start the osDetection Class function and return a value for the OS type.
+		osDetection.getOperatingSystemType();
+		System.out.println("FPS Cap:"); // UNPROGRAMMED CODE, CAP WILL BE 144 ON 1.2b
+	}
+
+	public static void RunGame() {
 		new Main();
 	}
 }
---
+++ zelda/osDetection.java
@@ -0,0 +1,75 @@
+package zelda;
+
+import javax.swing.*;
+import java.util.Locale;
+
+public class osDetection {
+
+    public enum OSType {
+        Windows, MacOS, Linux, Other
+    };
+
+    protected static OSType detectedOS;
+
+    // The purpose of this class is to detect the current Operating System and then figure out where to save
+    // The game at a later point when Save files are implemented.
+    // Unix/Mac would be kept in .cache/ZelUn for example compared to on Windows it'll be on %APPDATA%/ZelUn
+    // So far, calling getOperatingSystemType() in this class will only return the OS Type and whatever
+    // You put in the function header.
+
+    public static void getOperatingSystemType() {
+        if (detectedOS == null) {
+            String OS = System.getProperty("os.name", "generic").toLowerCase(Locale.ENGLISH);
+            if ((OS.indexOf("mac") >= 0) || (OS.indexOf("darwin") >= 0)) {
+                detectedOS = OSType.MacOS;
+                isMac();
+            } else if (OS.indexOf("win") >= 0) {
+                detectedOS = OSType.Windows;
+                isWin();
+            } else if (OS.indexOf("nux") >= 0) {
+                detectedOS = OSType.Linux;
+                isUnix();
+            } else {
+                detectedOS = OSType.Other;
+                autoDetectionFailed();
+            }
+        }
+    }
+
+    public static void isWin() {
+        System.out.println("Win32/Win64 System");
+        Main.RunGame();
+    }
+
+    public static void isMac() {
+        System.out.println("XNU System");
+        Main.RunGame();
+    }
+
+    public static void isUnix() {
+        System.out.println("I run arch");
+        Main.RunGame();
+    }
+
+    public static void autoDetectionFailed() {
+        System.out.println("Unknown System");
+
+        JOptionPane.showMessageDialog(null,
+                "System Detection Failed, Report it on Github!",
+                "Detection Failure",
+                JOptionPane.WARNING_MESSAGE);
+        try {
+            System.out.println("The osDetection script returned an error, please report it. We will now close the program.");
+            Thread.sleep(1000);
+            exitSystem();
+        } catch (InterruptedException ex) {
+            System.out.println("The osDetection script returned an error, please report it. We will now close the program.");
+            Thread.currentThread().interrupt();
+            exitSystem();
+        }
+    }
+
+    public static void exitSystem() {
+        Runtime.getRuntime().exit(0);
+    }
+}
